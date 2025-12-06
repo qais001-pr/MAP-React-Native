71eeb6fcffef61f9eaac930b1e56bc927450197e
Muhammad Qais <qaismuhammad742@gmail.com>
2025-12-07 00:12:51 +0500

    Entity Framework Added

---
+++ webapi/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        fypEntities _context = new fypEntities();
+
+        public HttpResponseMessage getStudents()
+        {
+            var Students = _context.Students.ToList();
+            return Request.CreateResponse(new
+            {
+                statusCode = HttpStatusCode.OK,
+                totalStdents = _context.Students.Count(),
+                studentList = _context.Students.ToList()
+            });
+        }
+        [HttpPost]
+        public HttpResponseMessage addStudent(Student s)
+        {
+            _context.Students.Add(s);
+            _context.SaveChanges();
+            return Request.CreateResponse("Data Saved");
+        }
+    }
+}
